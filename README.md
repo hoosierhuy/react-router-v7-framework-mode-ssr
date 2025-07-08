ef4701cfd64a7a1454913f24234148831487f497
Huy Nguyen <huynguyen@MacBookPro-896.lan>
2025-07-08 00:02:43 -0400

    Updated Readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Router v7 Framework Mode deployed to Cloudflare
 
-Deployed app: [https://react-router-app.hoosierhuy.workers.dev/products](https://react-router-app.hoosierhuy.workers.dev/products)
+Deployed app: [https://react-router-app.hoosierhuy.workers.dev](https://react-router-app.hoosierhuy.workers.dev)
 
 100 percent server-side rendered, including data fetching and mutations (POST request), with TypeScript and TailwindCSS 4.x.
 
--- app/routes/welcome.tsx
+++ app/routes/welcome.tsx
@@ -24,11 +24,12 @@ export default function Welcome({ ssr }: { ssr: string }) {
 				<h2 className="text-2xl center font-bold">Salve Amicis! </h2>
 				{ssr}
 				<p className="text-gray-600 mt-4">
-					Speak Now stadium music video Speak Now Aaron Dressner you need to
-					calm down London surprise Blank Space swiftie folk casette Marjorie
-					Willow merch debut exile co-writer Swift stained glass windows in my
-					mind deluxe [From the Vault] vocalist album of the year stadium
-					refrigerator light Cats bonus Fearless Our Song
+					(This is what is referred to as Taylor Swift Ipsum 🤣) Speak Now
+					stadium music video Speak Now Aaron Dressner you need to calm down
+					London surprise Blank Space swiftie folk casette Marjorie Willow merch
+					debut exile co-writer Swift stained glass windows in my mind deluxe
+					[From the Vault] vocalist album of the year stadium refrigerator light
+					Cats bonus Fearless Our Song
 				</p>
 			</div>
 		</main>
